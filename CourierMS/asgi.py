9927c7882244cd014bb7d761c2d354d22d318c22
unknown <laurencemmarete@gmail.com>
2024-02-23 11:55:39 +0300

    first commit

---
+++ CourierMS/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CourierMS project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CourierMS.settings')
+
+application = get_asgi_application()
